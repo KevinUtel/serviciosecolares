e47955686070ead06fdc629dd554ab266147cd41
Kevin <kaquinogar@utel.edu.mx>
2023-06-08 16:18:27 -0600

    archivos faltantes

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
-MONGODB_URI = `mongodb+srv://${NOTES_APP_MONGODB_HOST}@${NOTES_APP_MONGODB_DATABASE}`;
+MONGODB_URI = `mongodb+srv://kevaq:123456siete@utelsa.donokqz.mongodb.net/`;
 
 mongoose.connect(MONGODB_URI,{
     useUnifiedTopology: true,
