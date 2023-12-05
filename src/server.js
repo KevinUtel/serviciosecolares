2dd668c803505ebe63d5ed9fa3d97d39bd5af9d0
Kevin <kaquinogar@utel.edu.mx>
2023-12-05 09:26:18 -0600

    Present

--- src/server.js
+++ src/server.js
@@ -46,7 +46,6 @@ app.use((req, res, next) => {
 
 //routes
 app.use(require('./routes/index.routes'))
-app.use(require('./routes/notes.routes'))
 app.use(require('./routes/users.routes'))
 
 //static files
